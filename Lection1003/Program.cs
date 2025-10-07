9a61b18bc6e8e04a8f499d3da5d8846e5b536457
0109-04 <0109-04@0109-04>
2025-10-07 12:47:29 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ Lection1003/Program.cs
@@ -0,0 +1,12 @@
+﻿using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+Console.WriteLine("Разработка клиента");
+
+
+var connectionString = "";
+using IDbConnection connection = new SqlConnection(connectionString);
+string query = "INSERT INTO Category(title) OUTPUT INSERTED.CategoryId VALUES (@title)";
+
